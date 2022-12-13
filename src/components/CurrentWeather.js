0d0dd5c4b62ea7286b7ac4e0cc34c14241b6e417
Ar1kol <108225943+Ar1kol@users.noreply.github.com>
2022-12-13 21:14:55 +0200

    first commit

---
+++ src/components/CurrentWeather.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CurrentWeather = () => {
+  return <div className="weather-container">
+    <div className="weather-top">
+        <p className="weather-city">Haifa</p>
+        <p className="weather-description">Sunny</p>
+    </div>
+    <img  alt="weather" className="weather-icon" src=""/>
+  </div>;
+};
+
+export default CurrentWeather;
---
+++ src/utils/api.js
@@ -0,0 +1,8 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": process.env.REACT_APP_RAPIDAPI_KEY,
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  
